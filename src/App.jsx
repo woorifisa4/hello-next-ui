5c9ba3adc4b5c36f0f8626f2f91119459766993a
jeongmin07262 <dlwjdals0726@gmail.com>
2025-01-16 15:05:45 +0900

    ✨ NextUI Badge 컴포넌트 미리보기 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Navigation } from '@/components/Navigation';
+import { NextUIProvider } from '@nextui-org/react';
 import LandingPage from '@/pages/LandingPage';
 import CheckboxPreview from '@/pages/CheckboxPreview';
+import BadgePreview from '@/pages/BadgePreview';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-white">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/components/checkbox" element={<CheckboxPreview />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <NextUIProvider>
+      <BrowserRouter>
+        <div className="min-h-screen bg-white">
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/components/checkbox" element={<CheckboxPreview />} />
+            <Route path="/badge" element={<BadgePreview />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </NextUIProvider>
   );
 };
 
